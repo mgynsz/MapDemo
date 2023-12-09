5cd3b1bad7e18536c609d6750a79dfb0fa9a4522
mgynsz <112461285+mgynsz@users.noreply.github.com>
2023-12-09 12:06:32 +0900

    Add files via upload

---
+++ SwiftfulMapApp/SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,32 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by David Jang on 12/7/23.
+//
+
+import Foundation
+import MapKit
+
+struct Location: Identifiable, Equatable {
+        
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    // Identifiable
+    var id: String {
+        // name = "Colosseum"
+        // cityName = "Rome"
+        // id = "ColosseumRome"
+        name + cityName
+    }
+    
+    // Equatable
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+}
---
+++ SwiftfulMapApp/SwiftfulMapApp/Views/LocationDetailView.swift
@@ -0,0 +1,115 @@
+//
+//  LocationDetailView.swift
+//  SwiftfulMapApp
+//
+//  Created by David Jang on 12/8/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    let location: Location
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                imangSection
+                    .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 10)
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    titleSection
+                    Divider()
+                    descriptionSection
+                    Divider()
+                    mapLayer
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+        .background(.ultraThinMaterial)
+        .overlay(backButton, alignment: .topLeading)
+    }
+}
+
+extension LocationDetailView {
+    
+    private var imangSection: some View {
+        TabView {
+            ForEach(location.imageNames, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIDevice.current.userInterfaceIdiom == .pad ? nil :
+                            UIScreen.main.bounds.width)
+                    .clipped()
+            }
+        }
+        .frame(height: 500)
+        .tabViewStyle(PageTabViewStyle())
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(location.name)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Text(location.cityName)
+                .font(.title3)
+                .foregroundColor(.secondary)
+        }
+    }
+    
+    private var descriptionSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(location.description)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            if let url = URL(string: location.link) {
+                Link("Read more on Wikipedia", destination: url)
+                    .font(.headline)
+                    .tint(.blue)
+            }
+        }
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: .constant(MKCoordinateRegion(
+            center: location.coordinates,
+            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))),
+            annotationItems: [location]) { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .shadow(radius: 10)
+            }
+        }
+            .allowsHitTesting(false)
+            .aspectRatio(1, contentMode: .fit)
+            .cornerRadius(30)
+    }
+    
+    private var backButton: some View {
+        Button {
+            vm.sheetLocation = nil
+        } label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+                .padding(16)
+                .foregroundColor(.primary)
+                .background(.thickMaterial.opacity(0.7))
+                .cornerRadius(10)
+                .shadow(radius: 4)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    LocationDetailView(location: LocationsDataService.locations.first!)
+        .environmentObject(LocationsViewModel())
+}
